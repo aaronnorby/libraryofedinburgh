91c207adc432f28d1d5179ba2955da89b58aa29e
Aaron Norby <aaron.norby@gmail.com>
2016-05-28 19:49:15 -0700

    Print line on success

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "fmt"
   "log"
 
   "library-of-edinburgh/getTexts"
@@ -10,4 +11,5 @@ func main() {
   err := getTexts.GetAndSave(); if err != nil {
     log.Fatal(err)
   }
+  fmt.Println("treatise.txt created.")
 }
