93f89cbd7769b88d0291e425df72a0ef0fe49151
Aaron Norby <aaron.norby@gmail.com>
2016-06-18 10:34:33 -0700

    Return random seed so that texts can be reproduced later

--- bookmaker/bookmaker.go
+++ bookmaker/bookmaker.go
@@ -3,96 +3,107 @@
 package bookmaker
 
 import (
-  "bytes"
-  "fmt"
-  "io/ioutil"
-  "math"
-  "math/rand"
-  "os"
-  "strings"
-  "time"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
 )
 
-func MakeBook(fileName string) ([]byte, error) {
-  // makeBook takes a text file and generates a permutation of the words in that
-  // text. Note that each continuous whitespace counts as a word.
-  // At this point, it simply returns a randomly shuffled version of the text
-  file, err := os.Open(fileName)
-  defer file.Close()
-  if err != nil {
-    return nil, fmt.Errorf("error opening file %q: %v", fileName, err)
-  }
+func MakeBook(fileName string, seed int64) ([]byte, int64, error) {
+	// MakeBook takes a text file and generates a permutation of the words in that
+	// text. Note that each continuous whitespace counts as a word.
+	// The text returned is a permutation where the new position of each word is
+	// produced by a normal distribution whose mean is the original position of the
+	// word. It also returns the seed used to generate the permutation, so that the
+	// same book can be generated multiple times by supplying the seed. A seed value
+	// of 0 indicates that a new random seed needs to be generated.
+	file, err := os.Open(fileName)
+	defer file.Close()
+	if err != nil {
+		return nil, seed, fmt.Errorf("error opening file %q: %v", fileName, err)
+	}
 
-  // note: text is a []byte, as strings are just byte slices
-  text, err := ioutil.ReadAll(file)
-  if err != nil {
-    return nil, fmt.Errorf("error reading file %q: %v", fileName, err)
-  }
+	// note: text is a []byte, as strings are just byte slices
+	text, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, seed, fmt.Errorf("error reading file %q: %v", fileName, err)
+	}
 
-  // now we want a string slice of all the words, including newline 'words' (not
-  // all newlines - only those that break up grafs, ie runs of more than one \n)
-  // first, we count the number of newline characters, so we can add these back in
-  // later
-  newlinesCount := countNewlinesRun(text)
+	// now we want a string slice of all the words, including newline 'words' (not
+	// all newlines - only those that break up grafs, ie runs of more than one \n)
+	// first, we count the number of newline characters, so we can add these back in
+	// later
+	newlinesCount := countNewlinesRun(text)
 
-  words := strings.Fields(string(text))
+	words := strings.Fields(string(text))
 
-  // add newline chars back to our string slice
-  for runLength, count := range newlinesCount {
-    var run bytes.Buffer
-    for i := 0; i < runLength; i++ {
-      run.WriteString("\n")
-    }
-    runString := run.String()
-    for i := 0; i < count; i++ {
-      words = append(words, runString)
-    }
-  }
+	// add newline chars back to our string slice
+	for runLength, count := range newlinesCount {
+		var run bytes.Buffer
+		for i := 0; i < runLength; i++ {
+			run.WriteString("\n")
+		}
+		runString := run.String()
+		for i := 0; i < count; i++ {
+			words = append(words, runString)
+		}
+	}
 
-  // at this point, words in a string slice of all the words and newlines in the
-  // text.
-  shuffledWords := shuffle(words)
-  wordsAsByteSlice := []byte(strings.Join(shuffledWords, " "))
+	// at this point, words in a string slice of all the words and newlines in the
+	// text.
+	shuffledWords, seed := shuffle(words, seed)
+	wordsAsByteSlice := []byte(strings.Join(shuffledWords, " "))
 
-  return wordsAsByteSlice, nil
+	return wordsAsByteSlice, seed, nil
 }
 
 func countNewlinesRun(s []byte) map[int]int {
-  // find paragraph and heading breaks by counting runs of >1 newline character
-  runs := make(map[int]int)
-  var run int
-  for _, char := range s {
-    if string(char) == "\n" {
-      run++
-    } else if run > 1 {
-      runs[run]++
-      run = 0
-    } else {
-      run = 0
-    }
-  }
+	// find paragraph and heading breaks by counting runs of >1 newline character
+	runs := make(map[int]int)
+	var run int
+	for _, char := range s {
+		if string(char) == "\n" {
+			run++
+		} else if run > 1 {
+			runs[run]++
+			run = 0
+		} else {
+			run = 0
+		}
+	}
 
-  return runs
+	return runs
 }
 
-func shuffle(words []string) []string {
-  // shuffle performs a shiffer-yates shuffle on words and returns a new shuffled
-  // []string
-  // But instead of a pure random shuffle, we want to new position of a word to be
-  // based on its previous position. To do this, we use a normal distribution with
-  // a mean equal to the prior position of the word and a std dev equal to 10% of
-  // the total text word count.
-  var stdv float64 = 0.10 * float64(len(words)) // std deviation for normal dist used below
-  n := len(words)
-  result := make([]string, n)
-  rng := rand.New(rand.NewSource(time.Now().UnixNano()))
-  for i := 0; i < n; i++ {
-    newIdx := math.Floor(rng.NormFloat64() * stdv + float64(i) + 0.5)
-    j := int(math.Mod(math.Abs(newIdx), float64(n)))
-    if j != i {
-      result[i] = result[j]
-    }
-    result[j] = words[i]
-  }
-  return result
+func shuffle(words []string, seed int64) ([]string, int64) {
+	// shuffle performs a shiffer-yates shuffle on words and returns a new shuffled
+	// []string, also returning the random seed so that the same permutation can be
+	// generated again. A seed value of 0 indicates a new seed must be generated.
+	//
+	// Instead of a pure random shuffle, we want to new position of a word to be
+	// based on its previous position. To do this, we use a normal distribution with
+	// a mean equal to the prior position of the word and a std dev equal to 10% of
+	// the total text word count.
+	var stdv float64 = 0.10 * float64(len(words)) // std deviation for normal dist used below
+	n := len(words)
+	result := make([]string, n)
+
+	if seed == 0 {
+		seed = time.Now().UnixNano()
+	}
+
+	rng := rand.New(rand.NewSource(seed))
+	for i := 0; i < n; i++ {
+		newIdx := math.Floor(rng.NormFloat64()*stdv + float64(i) + 0.5)
+		j := int(math.Mod(math.Abs(newIdx), float64(n)))
+		if j != i {
+			result[i] = result[j]
+		}
+		result[j] = words[i]
+	}
+	return result, seed
 }
--- bookmaker/bookmaker_test.go
+++ bookmaker/bookmaker_test.go
@@ -1,49 +1,78 @@
 package bookmaker
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestCountNewlines(t *testing.T) {
-  words := "the\n\ncat and the\n hat"
-  numNewlines := countNewlinesRun([]byte(words))
-  expected := 1
-  if numNewlines[2] != expected {
-    t.Errorf("countNewlines(%v) == %v, expected %v", words, numNewlines[2], expected)
-  }
+	words := "the\n\ncat and the\n hat"
+	numNewlines := countNewlinesRun([]byte(words))
+	expected := 1
+	if numNewlines[2] != expected {
+		t.Errorf("countNewlines(%v) == %v, expected %v", words, numNewlines[2], expected)
+	}
 }
 
 func TestShuffle(t *testing.T) {
-  // Does not test uniformity of permutations. Only tests that shuffles in a row
-  // are distinct
-  words := []string{"one", "two", "three", "four", "five", "six"}
-  n := len(words)
-  result1 := shuffle(words)
-  result2 := shuffle(words)
-  if result1 == nil || result2 == nil {
-    t.Errorf("%v or %v is nil", result1, result2)
-  }
-  if len(result1) != n || len(result2) != n {
-    t.Errorf("%v not same length as %v", result1, result2)
-  }
-  same := true
-  for i := 0; i < len(result1); i++ {
-    if result1[i] != result2[i] {
-      same = false
-      break
-    }
-  }
-  if same {
-    t.Errorf("%v and %v are the same", result1, result2)
-  }
+	// Does not test uniformity of permutations. Only tests that shuffles in a row
+	// are distinct
+	words := []string{"one", "two", "three", "four", "five", "six"}
+	n := len(words)
+	result1, _ := shuffle(words, 0)
+	result2, _ := shuffle(words, 0)
+	if result1 == nil || result2 == nil {
+		t.Errorf("%v or %v is nil", result1, result2)
+	}
+	if len(result1) != n || len(result2) != n {
+		t.Errorf("%v not same length as %v", result1, result2)
+	}
+	same := true
+	for i := 0; i < len(result1); i++ {
+		if result1[i] != result2[i] {
+			same = false
+			break
+		}
+	}
+	if same {
+		t.Errorf("%v and %v are the same", result1, result2)
+	}
 }
 
 func TestMakeBook(t *testing.T) {
-  filename := "testdata/exampletext.txt"
-  textPerm, err := makeBook(filename)
-  if err != nil {
-    t.Errorf("Error in makeBook(%q): %v", filename, err)
-  }
-  // for go test -v show the permutation
-  t.Log(string(textPerm))
+	filename := "testdata/exampletext.txt"
+	textPerm1, _, err := MakeBook(filename, 0)
+	if err != nil {
+		t.Errorf("Error in makeBook(%q): %v", filename, err)
+	}
+	// for go test -v show the permutation
+	t.Log("\n" + string(textPerm1))
+
+	textPerm2, _, err := MakeBook(filename, 0)
+	if err != nil {
+		t.Errorf("Error in makeBook(%q), second call: %v", filename, err)
+	}
+
+	if string(textPerm1) == string(textPerm2) {
+		t.Errorf("Subsequent book permutations are the same. Should be different.")
+	}
+}
+
+func TestMakeBookWithSeed(t *testing.T) {
+	// test generating and reusing a random seed to reproduce a text
+	filename := "testdata/exampletext.txt"
+	text1, seed1, err := MakeBook(filename, 0)
+	if err != nil {
+		t.Errorf("Error in MakeBook(%q): %v", filename, err)
+	}
+	text2, seed2, err := MakeBook(filename, seed1)
+	if err != nil {
+		t.Errorf("Error in MakeBook(%q): %v", filename, err)
+	}
+
+	if seed1 != seed2 {
+		t.Errorf("Returned seed %q != %q, should be equal", seed1, seed2)
+	}
+	if string(text1) != string(text2) {
+		t.Errorf("Texts built with same seed not equal, should be equal")
+	}
 }
