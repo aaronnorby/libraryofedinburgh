7a9056bcc4b0ad05d0f25727b046f32244dbdfe1
Aaron Norby <aaron.norby@gmail.com>
2016-10-16 11:16:29 -0700

    Update header copy

--- webapp/src/components/SiteHeader.jsx
+++ webapp/src/components/SiteHeader.jsx
@@ -14,13 +14,16 @@ export default function SiteHeader(props) {
     permutations of Hume's book. If you leave the "seed" input blank, you'll get a
     random book. The position of a word in a given permutation is determined via a
     normal distribution with the mean being that word's position in the original
-    text. In addition to a book, you also get that book's "seed". This is the
-    random seed that was used to generate the book, and if you enter a seed into
+    text (paragraph breaks, while equal in number to the original, are distributed
+      evenly). In addition to a book, you also get that book's "seed". This is the
+    random seed that was used to generate the book (which is the time at which the
+      book was created represented as the number of nanoseconds since Jan 1 1970
+      UTC), and if you enter a seed into
     the input box and then ask for a book, you'll get that same book back.
         </p>
         <p>
-          The code for this site and the API server that makes the books is available <a href="">here</a>. 
-          Some tech info is available at the bottom of this page.
+          The code for this site and the API server that makes the books is
+    available <a href="https://github.com/aaronnorby/libraryofedinburgh">here</a>.
         </p>
       </section>
     </div>
